4d175b9802c139ef3609c0f7351380f9bef15540
Danesh Tolani <100458159+danesh-tolani@users.noreply.github.com>
2023-02-18 09:56:22 +0000

    learned throttling

--- throttling.js
+++ throttling.js
@@ -1,1 +1,32 @@
-// hello
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+function expensive() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+
+// throttle Method
+const throttle = (func, limit) => {
+    let flag = true;
+     let counter = 0;
+    return function () {
+        if(flag) {
+            func()
+            console.log("Triggered: ", counter++)
+            flag = false;
+            setTimeout(() => {
+                flag=true
+            }, limit)
+        }
+    }
+}
+
+
+// throttled expensive function
+const betterExpensiveFunction = throttle(expensive, 1000)
+
+
+// window.addEventListener("resize", expensive);
+window.addEventListener("resize", betterExpensiveFunction);
