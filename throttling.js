f7e1cfb2f299801cf799ac131c4a76e8022ac2aa
Danesh Tolani <100458159+danesh-tolani@users.noreply.github.com>
2023-02-18 10:02:17 +0000

    advance throttling

--- throttling.js
+++ throttling.js
@@ -1,20 +1,41 @@
 const heightOutput = document.querySelector("#height");
 const widthOutput = document.querySelector("#width");
 
-function expensive() {
-  heightOutput.textContent = window.innerHeight;
-  widthOutput.textContent = window.innerWidth;
-}
+// function expensive() {
+//   heightOutput.textContent = window.innerHeight;
+//   widthOutput.textContent = window.innerWidth;
+// }
 
 
 // throttle Method
+// const throttle = (func, limit) => {
+//     let flag = true;
+//      let counter = 0;
+//     return function () {
+//         if(flag) {
+//             func()
+//             console.log("Triggered: ", counter++)
+//             flag = false;
+//             setTimeout(() => {
+//                 flag=true
+//             }, limit)
+//         }
+//     }
+// }
+
+// advanced throttle method
+function expensive(arguments) {
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+  }
+
 const throttle = (func, limit) => {
     let flag = true;
-     let counter = 0;
     return function () {
+        let context = this
+        args = arguments
         if(flag) {
-            func()
-            console.log("Triggered: ", counter++)
+            func.apply(context, args)
             flag = false;
             setTimeout(() => {
                 flag=true
@@ -22,7 +43,6 @@ const throttle = (func, limit) => {
         }
     }
 }
-
 
 // throttled expensive function
 const betterExpensiveFunction = throttle(expensive, 1000)
